b8b87872f89f2d14d4be401a18df932ba35dfe85
Jackson Kauflin <themarduk28@gmail.com>
2019-09-26 22:03:13 -0400

    Updated pshell user profile and added new 1809 and 1903 sct baselines

--- cmder/config/user_profile.ps1
+++ cmder/config/user_profile.ps1
@@ -1,4 +1,9 @@
+# Copy script to profile
 Copy-Item "$env:userprofile\Desktop\Script\script.ps1" `
 "$env:userprofile\Documents\WindowsPowershell\Microsoft.PowerShell_profile.ps1"
 
+# Delete unecessary git folder
+Remove-Item "$env:userprofile\Desktop\Script\.git" -Recurse -Force
+
+# Run script
 . $profile
