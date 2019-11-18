f238333cb90e9301e9d68377af79e8442495a57c
Jackson <themarduk28@gmail.com>
2019-11-17 23:19:09 -0500

    made all registry settings look and function good

--- script.ps1
+++ script.ps1
@@ -92,10 +92,8 @@ function Set-SOLogonMessage {
     Set-LocalUser $env:username -Password $pass
 
     # Set logon message
-    New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name legalnoticecaption `
-    -PropertyType String -Value "Username: $env:username" -Force -InformationAction SilentlyContinue
-    New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name legalnoticetext `
-    -PropertyType String -Value "Password: abc123ABC123@@" -Force -InformationAction SilentlyContinue
+    New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Force | New-ItemProperty -Name "legalnoticecaption" -PropertyType "String" -Value "Username: $env:username" -Force
+    New-Item -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Force | New-ItemProperty -Name "legalnoticetext" -PropertyType "String" -Value "Password: abc123ABC123@@" -Force
 }
 
 # Add to progress log
@@ -114,12 +112,9 @@ function Import-SOAlias {
 
 # Setup autologon again for our user for convenience
 function Set-SOAutoLogon {
-    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUserName `
-    -PropertyType String -Value "$env:username" -Force
-    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword `
-    -PropertyType String -Value "abc123ABC123@@" -Force
-    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogon `
-    -PropertyType String -Value "1" -Force
+    New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Force | New-ItemProperty -Name "DefaultUserName" -PropertyType "String" -Value "$env:username" -Force
+    New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Force | New-ItemProperty -Name "DefaultPassword" -PropertyType "String" -Value "abc123ABC123@@" -Force
+    New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Force | New-ItemProperty -Name "AutoAdminLogon" -PropertyType "String" -Value "1" -Force
 }
 
 # SCT Baselines
@@ -137,13 +132,7 @@ function Import-SCT {
     }
 
     # Allow cmder and stop scoring, etc. to actually run lol
-    if ((Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" `
-    -Name ValidateAdminCodeSignatures) -eq "1") {
-        New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System `
-        -Name ValidateAdminCodeSignatures -PropertyType DWord -Value "0" -Force
-    }
-    New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name EnableUIADesktopToggle `
-    -PropertyType DWord -Value "0" -Force
+    Fix-Programs
 
     # Set logon message
     Set-SOLogonMessage
@@ -395,10 +384,8 @@ function Remove-Malware {
 
 # Disable remote desktop
 function Disable-RemoteDesktop {
-    New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" `
-    -PropertyType DWord -Value "1" -Force
-    New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name "fAllowToGetHelp" `
-    -PropertyType DWord -Value "0" -Force
+    New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Force | New-ItemProperty -Name "fDenyTSConnections" -PropertyType "DWord" -Value "1" -Force
+    New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Force | New-ItemProperty -Name "fAllowToGetHelp" -PropertyType "DWord" -Value "0" -Force
 
     Add-SOProgress "Remote Desktop disabled"
     Write-Output "Disable remote desktop."
@@ -406,15 +393,8 @@ function Disable-RemoteDesktop {
 
 # Secure screensaver with password gamer
 function Protect-Screensaver {
-    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows" -Name "Control Panel" -Force
-    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Control Panel" -Name "Desktop" -Force
-    New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Control Panel\Desktop" `
-    -Name ScreenSaverIsSecure -PropertyType DWord -Value "1" -Force
-
-    New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows" -Name "Control Panel"
-    New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Control Panel" -Name "Desktop" -Force
-    New-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Control Panel\Desktop" `
-    -Name ScreenSaverIsSecure -PropertyType DWord -Value "1" -Force
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Control Panel\Desktop" -Force | New-ItemProperty -Name "ScreenSaverIsSecure" -PropertyType "DWord" -Value "1" -Force
+    New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Control Panel\Desktop" -Force | New-ItemProperty -Name "ScreenSaverIsSecure" -PropertyType "DWord" -Value "1" -Force
 
     Add-SOProgress "Screensaver secured with password"
     Write-Output "Secured screensaver with a password."
@@ -432,18 +412,14 @@ function Clear-Hosts {
 function Set-FirefoxConfig {
     # 64-bit
     if ((Test-Path "$env:programfiles\Mozilla Firefox") -eq $True) {
-        Copy-Item "$compfiles\firefox_config\override.ini" "$env:programfiles\Mozilla Firefox\browser\override.ini" `
-        -Force
+        Copy-Item "$compfiles\firefox_config\override.ini" "$env:programfiles\Mozilla Firefox\browser\override.ini" -Force
         Copy-Item "$compfiles\firefox_config\mozilla.cfg" "$env:programfiles\Mozilla Firefox\mozilla.cfg" -Force
-        Copy-Item "$compfiles\firefox_config\local-settings.js" `
-        "$env:programfiles\Mozilla Firefox\defaults\pref\local-settings.js" -Force
+        Copy-Item "$compfiles\firefox_config\local-settings.js" "$env:programfiles\Mozilla Firefox\defaults\pref\local-settings.js" -Force
     } else {
         # 32 bit
-        Copy-Item "$compfiles\firefox_config\override.ini" `
-        "$env:programfiles(x86)\Mozilla Firefox\browser\override.ini" -Force
+        Copy-Item "$compfiles\firefox_config\override.ini" "$env:programfiles(x86)\Mozilla Firefox\browser\override.ini" -Force
         Copy-Item "$compfiles\firefox_config\mozilla.cfg" "$env:programfiles(x86)\Mozilla Firefox\mozilla.cfg" -Force
-        Copy-Item "$compfiles\firefox_config\local-settings.js" `
-        "$env:programfiles(x86)\Mozilla Firefox\defaults\pref\local-settings.js" -Force
+        Copy-Item "$compfiles\firefox_config\local-settings.js" "$env:programfiles(x86)\Mozilla Firefox\defaults\pref\local-settings.js" -Force
     }
 
     Add-SOProgress "Firefox config files copied"
@@ -563,10 +539,8 @@ function Import-IERegistry {
 # Enable internet explorer
 function Enable-InternetExplorer {
     # Enable IE
-    Enable-WindowsOptionalFeature -Online -FeatureName 'Internet-Explorer-Optional-x86' -all `
-    -ErrorAction SilentlyContinue
-    Enable-WindowsOptionalFeature -Online -FeatureName 'Internet-Explorer-Optional-amd64' -all `
-    -ErrorAction SilentlyContinue
+    Enable-WindowsOptionalFeature -Online -FeatureName 'Internet-Explorer-Optional-x86' -all -ErrorAction SilentlyContinue
+    Enable-WindowsOptionalFeature -Online -FeatureName 'Internet-Explorer-Optional-amd64' -all -ErrorAction SilentlyContinue
 
     Add-SOProgress "Enabled Internet Explorer"
     Write-Output "Enabled the gamer internet explorer."
@@ -611,8 +585,7 @@ function Enable-Users {
 
 # enable uac because that would be a good idea though its already enabled by default but whatever frick off
 function Enable-UAC {
-    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" `
-    -PropertyType DWord -Value "1" -Force
+    New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Force | New-ItemProperty -Name "EnableLUA" -PropertyType "DWord" -Value "1" -Force
 
     Add-SOProgress "UAC Enabled"
     Write-Output "Enabled UAC"
@@ -753,8 +726,7 @@ function Open-StopScoring {
 
 # Enable remote desktop
 function Enable-RemoteDesktop {
-    New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" `
-    -PropertyType DWord -Value "0" -Force
+    New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Force | New-ItemProperty -Name "fDenyTSConnections" -PropertyType "DWord" -Value "0" -Force
 
     Add-SOProgress "Remote Desktop enabled"
     Write-Output "Enable remote desktop."
@@ -810,8 +782,8 @@ function Install-Chocolatey {
 
 # Allow cmder, stop scoring, etc. to work lol
 function Fix-Programs {
-    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ValidateAdminCodeSignatures" -PropertyType "DWord" -Value "0" -Force
-    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableUIADesktopToggle" -PropertyType "DWord" -Value "0" -Force
+    New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Force | New-ItemProperty -Name "ValidateAdminCodeSignatures" -PropertyType "DWord" -Value "0" -Force
+    New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Force | New-ItemProperty -Name "EnableUIADesktopToggle" -PropertyType "DWord" -Value "0" -Force
 
     Write-Output "Cmder, stop scoring, etc. fixed."
 }
@@ -821,8 +793,8 @@ function Start-InitialSetup {
     # Install Carbon and PSWindowsUpdate modules
 
     # Disable Use FIPS compliant checksums (Allow install of modules)
-    New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\FIPSAlgorithmPolicy" -Name "Enabled" -PropertyType "DWord" -Value "0" -Force
-    New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control" -Name "FIPSAlgorithmPolicy" -PropertyType "DWord" -Value "0" -Force
+    New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\FIPSAlgorithmPolicy" -Force | New-ItemProperty -Name "Enabled" -PropertyType "DWord" -Value "0" -Force
+    New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control" -Force | New-ItemProperty -Name "FIPSAlgorithmPolicy" -PropertyType "DWord" -Value "0" -Force
 
     Set-PSRepository -Name "PSGallery" -InstallationPolicy "Trusted"
     Install-PackageProvider -Name "NuGet" -MinimumVersion "2.8.5.201" -Force
@@ -830,8 +802,8 @@ function Start-InitialSetup {
     Install-Module -Name "PSWindowsUpdate" -AllowClobber -Force
 
     # Re-enable Use FIPS compliant algorithms
-    New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\FIPSAlgorithmPolicy" -Name "Enabled" -PropertyType "DWord" -Value "1" -Force
-    New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control" -Name "FIPSAlgorithmPolicy" -PropertyType "DWord" -Value "1" -Force
+    New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\FIPSAlgorithmPolicy" -Force | New-ItemProperty -Name "Enabled" -PropertyType "DWord" -Value "1" -Force
+    New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control" -Force | New-ItemProperty -Name "FIPSAlgorithmPolicy" -PropertyType "DWord" -Value "1" -Force
 }
 
 # Run initial setup if it hasnt been run
