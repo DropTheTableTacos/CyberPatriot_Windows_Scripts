702592472569b2c0d4266e09bdc99ab38e917ca8
Jackson <themarduk28@gmail.com>
2019-12-01 04:34:45 -0500

    nvm that feature thing ig? also couldnt figure out how to yeet a commit off the face of the earth so here we are

--- disable_features.ps1
+++ disable_features.ps1
@@ -1,5 +1,5 @@
 $feature_list = Import-Lists features
 
 $feature_list.foreach{
-    if (Get-WindowsOptionalFeature -Online -FeatureName $_ | Where-Object State -notmatch "Disable") {Disable-WindowsOptionalFeature -FeatureName $_.Name -Online -NoRestart}
+    Disable-WindowsOptionalFeature -FeatureName $_.Name -Online -NoRestart
 }
