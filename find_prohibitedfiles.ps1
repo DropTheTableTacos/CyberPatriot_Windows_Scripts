a5c227059b2660842876d7a314d8197bb2e7dfdd
Jackson <themarduk28@gmail.com>
2020-01-18 19:32:52 -0500

    added new def_files list, added some epic functionality to prohib. files, random new serv and feature

--- find_prohibitedfiles.ps1
+++ find_prohibitedfiles.ps1
@@ -2,96 +2,55 @@
 Remove-Item "C:\stinky_files.txt" -Force -ErrorAction SilentlyContinue | Out-Null
 
 $ext = Import-Lists extensions | Where-Object Action -eq "Find"
+$def_files = Import-Lists win10_defaultfiles
 $pattern = Import-Lists sensinfo_patterns
 
 # Initial finding txt and zip files cause they most common
 Clear-Host
-Write-Output "Finding .zip and .txt files first..."
+Write-Output "Finding common file types first..."
 
-$cringe = "*.zip","*.txt"
+$cringe = "*.zip","*.txt","*.tar.gz"
 $cringe.foreach{
-    $files += Get-ChildItem -Path "C:\" -Filter $_ -Recurse -Attributes !Directory+!System,Hidden | Where-Object FullName -notmatch "C:\\Users\\$env:USERNAME\\Desktop\\Script" | Where-Object FullName -notmatch "C:\\CyberPatriot"
+    $old_files += Get-ChildItem -Path "C:\" -Filter $_ -Recurse -Attributes !Directory+!System,Hidden | Where-Object FullName -notmatch "C:\\Users\\$env:USERNAME\\Desktop\\Script" | Where-Object FullName -notmatch "C:\\CyberPatriot"
 }
+
+# Compare to default files
+$files = (Compare-Object $old_files.FullName $def_files.Name).InputObject
 
 # Takeown and output to file
 $files.foreach{
-    if ((Test-Path "$_") -eq $true) {
-        takeown /f $_.FullName
-        icacls $_.FullName /grant ${env:USERNAME}:`(F`)
-    }
+    takeown /f $_.FullName
+    icacls $_.FullName /grant ${env:USERNAME}:`(F`)
 }
 $files.FullName >> "C:\stinky_files.txt"
 
 Start-Process "C:\stinky_files.txt"
 
-# Delete ones you possibly find
-while ($true) {
-    Clear-Host
-    if ($null -ne $answer) {
-        Write-Output "$answer has been deleted as hard as an Armenian in 1915."
-        Write-Output "`n"
-    }
-
-    Write-Output "Found .zip and .txt files..."
-    Write-Output "`n"
-    $answer = Read-Host "Enter the path to a file to YEET it off the VM"
-
-    if ($answer -eq "n") {
-        break
-    }
-
-    takeown /f "$answer"
-    icacls "$answer" /grant ${env:USERNAME}:`(F`)
-    "$answer" >> "C:\deleted_files.txt"
-    Remove-Item "$answer" -Force
-}
-
 # Find rest of extensions
-Remove-Item "C:\stinky_files.txt" -Force -ErrorAction SilentlyContinue | Out-Null
-
 Write-Output "Finding rest of extensions..."
+
+$old_files = $null
+$files = $null
 
 $ext.foreach{
     # Get the files
-    $files += Get-ChildItem -Path "C:\" -Filter $_.Name -Recurse -Attributes !Directory+!System,Hidden | Where-Object FullName -notmatch "C:\\Users\\$env:USERNAME\\Desktop\\Script" | Where-Object FullName -notmatch "C:\\CyberPatriot"
-
-    # Takeown and output to file
-    $files.foreach{
-        if ((Test-Path "$_") -eq $true) {
-            takeown /f $_.FullName
-            icacls $_.FullName /grant ${env:USERNAME}:`(F`)
-        }
-    }
-
-    $files.FullName >> "C:\stinky_files.txt"
+    $old_files += Get-ChildItem -Path "C:\" -Filter $_.Name -Recurse -Attributes !Directory+!System,Hidden | Where-Object FullName -notmatch "C:\\Users\\$env:USERNAME\\Desktop\\Script" | Where-Object FullName -notmatch "C:\\CyberPatriot"
 }
+
+# Compare to default files
+$files = (Compare-Object $old_files.FullName $def_files.Name).InputObject
+
+# Takeown and output to file
+$files.foreach{
+    takeown /f $_.FullName
+    icacls $_.FullName /grant ${env:USERNAME}:`(F`)
+}
+$files.FullName >> "C:\stinky_files.txt"
 
 Start-Process "C:\stinky_files.txt"
 
-# Delete ones you possibly find
-while ($true) {
-    Clear-Host
-    if ($null -ne $answer) {
-        Write-Output "$answer has been deleted as hard as an Armenian in 1915."
-        Write-Output "`n"
-    }
-
-    Write-Output "Found cringe files..."
-    Write-Output "`n"
-    $answer = Read-Host "Enter the path to a file to YEET it off the VM"
-
-    if ($answer -eq "n") {
-        break
-    }
-
-    takeown /f "$answer"
-    icacls "$answer" /grant ${env:USERNAME}:`(F`)
-    "$answer" >> "C:\deleted_files.txt"
-    Remove-Item "$answer" -Force
-}
-
+<#
 # Find txt files matching patterns
-Remove-Item "C:\stinky_files.txt" -Force -ErrorAction SilentlyContinue | Out-Null
 
 $txt = Get-ChildItem -Path "C:\" -Filter *.txt -Recurse -Attributes !Directory+!System,Hidden | Where-Object FullName -notmatch "C:\\Users\\$env:USERNAME\\Desktop\\Script" | Where-Object FullName -notmatch "C:\\CyberPatriot"
 
@@ -102,30 +61,7 @@ $pattern.foreach{
 
 Start-Process "C:\stinky_files.txt"
 
-# Delete ones you possibly find
-while ($true) {
-    Clear-Host
-    if ($null -ne $answer) {
-        Write-Output "$answer has been deleted as hard as an Armenian in 1915."
-        Write-Output "`n"
-    }
-
-    Write-Output "Found sketchy .txt files..."
-    Write-Output "`n"
-    $answer = Read-Host "Enter the path to a file to YEET it off the VM"
-
-    if ($answer -eq "n") {
-        break
-    }
-
-    takeown /f "$answer"
-    icacls "$answer" /grant ${env:USERNAME}:`(F`)
-    "$answer" >> "C:\deleted_files.txt"
-    Remove-Item "$answer" -Force
-}
-
 # Find bad user owned files (doesnt work rn)
-<#
 Remove-Item "C:\stinky_files.txt" -Force -ErrorAction SilentlyContinue | Out-Null
 
 $badusers.foreach{
--- remove_prohibitedfiles.ps1
+++ remove_prohibitedfiles.ps1
@@ -1,20 +1,24 @@
 $ext = Import-Lists extensions | Where-Object Action -eq "Delete"
+$def_files = Import-Lists win10_defaultfiles
+$path = "$desktop\CringeFiles"
 
-Write-Output "Files that have been deleted:`n" >> "C:\deleted_files.txt"
+mkdir "$path"
+
+Write-Output "Files that have been moved:`n" >> "C:\moved_files.txt"
 
 $ext.foreach{
     # Get the files
-    $files += Get-ChildItem -Path "C:\" -Filter $_.Name -Recurse -Attributes !Directory+!System,Hidden | Where-Object FullName -notmatch "C:\\Users\\$env:USERNAME\\Desktop\\Script" | Where-Object FullName -notmatch "C:\\CyberPatriot"
-
-    # Takeown and yeet them off the VM
-    $files.foreach{
-        if ((Test-Path "$_") -eq $true) {
-            takeown /f $_.FullName
-            icacls $_.FullName /grant ${env:USERNAME}:`(F`)
-        }
-    }
-
-    $files.FullName | Remove-Item -Force
-    $files.FullName >> "C:\deleted_files.txt"
+    $old_files += Get-ChildItem -Path "C:\" -Filter $_.Name -Recurse -Attributes !Directory+!System,Hidden | Where-Object FullName -notmatch "C:\\Users\\$env:USERNAME\\Desktop\\Script" | Where-Object FullName -notmatch "C:\\CyberPatriot"
 }
 
+# Compare to default files
+$files = (Compare-Object $old_files.FullName $def_files.Name).InputObject
+
+# Takeown and output to file
+$files.foreach{
+    takeown /f $_.FullName
+    icacls $_.FullName /grant ${env:USERNAME}:`(F`)
+}
+
+$files | Move-Item $_.FullName $path\$_.Name -Force
+$files.FullName >> "C:\moved_files.txt"
