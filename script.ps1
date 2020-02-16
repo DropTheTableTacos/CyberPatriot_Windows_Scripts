2ebb7c4b3e5f9494c1d2bea850b4f86c90e8f571
Jackson <themarduk28@gmail.com>
2020-02-16 14:50:55 -0500

    fixed running auto_seperate functions

--- script.ps1
+++ script.ps1
@@ -4,11 +4,6 @@
     Invoke-Expression -Command "$_.Name"
 }
 
-($functions | Where-Object Type -match "^Auto_Seperate$").foreach{
-    Invoke-Expression -Command "$_.Name"
-}
-
-<#
 # Run gamer seperate scripts
 Disable-Features
 Install-Programs
@@ -17,4 +12,3 @@ Disable-Services
 Update-Windows
 Start-CiscatRegistry
 Import-IERegistry
-#>
